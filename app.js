29131c7cf183ba3d518ee34dae47704147f3c8f4
Alyssa Sanchez <93028359+bellatrixx01@users.noreply.github.com>
2023-02-01 01:27:27 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const inputBox = document.getElementById("input")
+const inputTask = document.getElementById("input")
 const submitBtn = document.getElementById("submit-btn")
 const listDisplay = document.getElementById("list-display")
 
-inputBox.addEventListener("keypress", function (e) {
+inputTask.addEventListener("keypress", function (e) {
 	if (event.key === "Enter") {
-		let inputValue = inputBox.value
+		let inputValue = inputTask.value
 		//create list item
 		let listItem = document.createElement("li")
 		listItem.innerHTML = inputValue + "<button>set</button>"
 		listDisplay.appendChild(listItem)
-		inputBox.value = ""
+		inputTask.value = ""
 	}
 })
